124f97804c41bc89bac84a5e0a5acafd47ecd1c7
Diego Hernandez <hernandezr.diego.p8@gmail.com>
2025-10-11 18:29:44 -0600

    Nuevas dependencias

---
+++ src/main/java/com/example/vida_submarina/VidaSubmarinaApplication.java
@@ -0,0 +1,13 @@
+package com.example.vida_submarina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidaSubmarinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidaSubmarinaApplication.class, args);
+	}
+
+}
